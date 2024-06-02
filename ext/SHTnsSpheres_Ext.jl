78408817bbb33110953851b7bd7e9ea49da0adef
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-06-02 15:05:59 +0200

    HPE initial state

--- ext/SHTnsSpheres_Ext.jl
+++ ext/SHTnsSpheres_Ext.jl
@@ -2,8 +2,18 @@ module SHTnsSpheres_Ext
 
 using MutatingOrNot: void, Void
 using SHTnsSpheres: SHTnsSphere, analysis_scalar!, analysis_vector!
+using CFPlanets: ShallowTradPlanet, coriolis
+using CFDomains: shell
 using CFHydrostatics: CFHydrostatics
-import CFHydrostatics: initial_HPE_HV
+import CFHydrostatics: initial_HPE_HV, HPE
+
+function HPE(params, sph::SHTnsSphere, vcoord, geopotential, gas)
+    (; radius, Omega), (; lon, lat) = params, sph
+    planet = ShallowTradPlanet(radius, Omega)
+    fcov = coriolis.(Ref(planet), lon, lat)
+    Phis = geopotential.(lon, lat)
+    return HPE(vcoord, planet, shell(params.nz, sph), gas, fcov, Phis)
+end
 
 ## these "constructors" seem to help with type stability
 vector_spec(spheroidal, toroidal) = (; spheroidal, toroidal)
--- src/CFHydrostatics.jl
+++ src/CFHydrostatics.jl
@@ -1,9 +1,19 @@
 module CFHydrostatics
 
-import CFDomains
-using ClimFluids: SimpleFluid
+using CFPlanets: ShallowTradPlanet
+using CFDomains: CFDomains
+using ClimFluids: AbstractFluid, SimpleFluid
 
 include("julia/vertical_coordinate.jl")
+
+struct HPE{F, Coord, Domain, Fluid, TwoDimScalar}
+    vcoord::Coord
+    planet::ShallowTradPlanet{F}
+    domain::Domain
+    gas::Fluid
+    fcov::TwoDimScalar # covariant Coriolis factor = f(lat)*radius^2
+    Phis::TwoDimScalar # surface geopotential
+end
 
 include("julia/initialize.jl")
 
