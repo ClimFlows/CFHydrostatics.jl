4561b46bfb2e0275ae81e20fc310969e9c873f6c
Documenter.jl <documenter@juliadocs.github.io>
2024-06-02 10:22:12 +0000

    build based on d3f77eb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CFHydrostatics","category":"page"},{"location":"#CFHydrostatics","page":"Home","title":"CFHydrostatics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CFHydrostatics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CFHydrostatics]","category":"page"}]
+}
