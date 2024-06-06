e8e41f9d70d56a676a3cd060fe8ffb1cfbd0249a
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-06-06 22:19:21 +0200

    Detect rank mismatches

--- src/julia/remap_HPE.jl
+++ src/julia/remap_HPE.jl
@@ -6,6 +6,9 @@ using ManagedLoops: @loops, @vec
 using CFDomains: VHLayout, HVLayout
 using CFTransport: CFTransport, OneDimFV, minmod_simd
 
+const FV{Rank, Dim, kind} = OneDimFV{kind, Dim, Rank} # to dispatch on FV{Rank}
+const AA{Rank, T} = AbstractArray{T, Rank} # to dispatch on AA{Rank}
+
 # convention:
 #      fun(non-fields, output fields..., #==# scratch #==# input fields...)
 # or   fun(non-fields, output fields..., #==# input fields...)
@@ -23,7 +26,6 @@ function remap_density!(mgr, vanleer, new_massq, #==# fluxq, dq, q, #==# massq, 
     dq = slopes!(mgr, vanleer, dq, #==# q)
     fluxq = fluxes!(mgr, vanleer, fluxq, #==# dq, q, mass, flux)
     new_massq = update_density!(mgr, vanleer, new_massq, #==# fluxq, massq)
-    # @info "remap_density!" absmax(new_massq) absmax(fluxq) absmax(dq) absmax(q) absmax(massq) absmax(mass) absmax(flux)
     return new_massq
 end
 
@@ -31,7 +33,6 @@ function remap_scalar!(mgr, vanleer, new_q, #==# fluxq, dq, #==# q, mass, flux)
     dq = slopes!(mgr, vanleer, dq, #==# q)
     fluxq = fluxes!(mgr, vanleer, fluxq, #==# dq, q, mass, flux)
     new_q = update_scalar!(mgr, vanleer, new_q, #==# fluxq, q, flux, mass)
-    # @info "remap_scalar!" absmax(new_q) absmax(fluxq) absmax(dq) absmax(q) absmax(mass) absmax(flux)
     return new_q
 end
 
@@ -39,6 +40,7 @@ end
 
 concentrations!(mgr, ::Void, massq, mass) = concentrations!(mgr, similar(massq), massq, mass)
 function concentrations!(mgr, q, massq, mass)
+    @assert axes(q) == axes(massq)
     CFTransport.concentrations!(mgr, q, massq, mass)
     return q
 end
@@ -47,14 +49,15 @@ end
 
 slopes!(mgr, vanleer, ::Void, q) = slopes!(mgr, vanleer, similar(q), q)
 
-function slopes!(mgr, vanleer, dq, q)
+function slopes!(mgr, vanleer::FV{N}, dq::AA{N}, q::AA{N}) where N
+    @assert axes(dq) == axes(q)
     CFTransport.slopes!(mgr, vanleer, dq, q)
     zero_bottom_top!(mgr, vanleer, dq)
     return dq
 end
 
-zero_bottom_top!(mgr, ::OneDimFV{<:Any,1,2}, q) = zero_bottom_top_VH!(mgr, q)
-zero_bottom_top!(mgr, ::OneDimFV{<:Any,2,2}, q) = zero_bottom_top_HV!(mgr, q)
+zero_bottom_top!(mgr, ::FV{2,1}, q::AA{2}) = zero_bottom_top_VH!(mgr, q)
+zero_bottom_top!(mgr, ::FV{2,2}, q::AA{2}) = zero_bottom_top_HV!(mgr, q)
 
 @loops function zero_bottom_top_HV!(_, q)
     let range = axes(q,1)
@@ -78,7 +81,8 @@ end
 
 fluxes!(mgr, vanleer, ::Void, q, dq, mass, flux) = fluxes!(mgr, vanleer, similar(flux), q, dq, mass, flux)
 
-function fluxes!(mgr, vanleer, fluxq, dq, q, mass, flux)
+function fluxes!(mgr, vanleer::FV{N}, fluxq::AA{N}, dq::AA{N}, q::AA{N}, mass::AA{N}, flux::AA{N}) where N
+    @assert axes(fluxq) == axes(flux)
     CFTransport.fluxes!(mgr, vanleer, fluxq, dq, q, mass, flux)
     zero_bottom_top!(mgr, vanleer, fluxq)
     # @info "fluxes!" absmax(flux) absmax(fluxq)
@@ -90,7 +94,8 @@ end
 update_density!(mgr, vanleer, ::Void, #==# fluxq, massq) =
     update_density!(mgr, vanleer, similar(massq), #==# fluxq, massq)
 
-function update_density!(mgr, vanleer, massqnew, #==# fluxq, massqnow)
+function update_density!(mgr, vanleer::FV{N}, massqnew::AA{N}, #==# fluxq::AA{N}, massqnow::AA{N}) where N
+    @assert axes(massqnew) == axes(massqnow)
     CFTransport.FV_update!(mgr, vanleer, massqnew, massqnow, fluxq)
     return massqnew
 end
@@ -98,13 +103,15 @@ end
 update_scalar!(mgr, vanleer, ::Void, #==# fluxq, qnow, flux, mass) =
     update_scalar!(mgr, vanleer, similar(qnow), #==# fluxq, qnow, flux, mass)
 
-function update_scalar!(mgr, vanleer, qnew, #==# fluxq, qnow, flux, mass)
+function update_scalar!(mgr, vanleer::FV{N}, qnew::AA{N}, #==# fluxq::AA{N}, qnow::AA{N}, flux::AA{N}, mass::AA{N}) where N
+    @assert axes(qnew) == axes(qnow)
     CFTransport.FV_update!(mgr, vanleer, qnew, qnow, fluxq, flux, mass) # correct!
     return qnew
 end
 
 update_mass!(mgr, ::Void, newmass) = update_mass!(mgr, similar(newmass), newmass)
 function update_mass!(mgr, mass, newmass)
+    @assert axes(mass) == axes(newmass)
     update_mass_(mgr, mass, newmass)
     return mass
 end
