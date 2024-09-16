49e37e6636ad771065770e6cf9b1acda9f19f35e
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-09-16 21:50:50 +0200

    Fix divergence

--- src/julia/voronoi_diagnostics.jl
+++ src/julia/voronoi_diagnostics.jl
@@ -5,67 +5,74 @@ using CFDomains: primal_lonlat_from_cov!
 using CFPlanets: lonlat_from_cov
 using ManagedLoops: @with, @vec, @unroll
 
-using ..Stencils: stencil, dot_product
+using ..Stencils: dot_product, centered_flux, divergence
 
-function diagnostics()
-    return CookBook(;
-        ucov,
-        mass_air,
-        mass_consvar,
-        ulonlat_i,
-        ulon,
-        ulat,
-        pressure_i,
-        pressure,
-        surface_pressure_i,
-        surface_pressure,
-        consvar_i,
-        consvar,
-        temperature_i,
-        temperature,
-        geopotential_i,
-        geopotential,
-        gradp,
-        ugradp_i,
-        ugradp,
-    )
-end
+diagnostics() = CookBook(;
+    ulon,
+    ulat,
+    pressure,
+    consvar,
+    surface_pressure,
+    temperature,
+    geopotential,
+    ugradp,
+    dmass_air,
+    Omega,
+    ucov_e,
+    mass_air_i,
+    mass_consvar_i,
+    ulonlat_i,
+    pressure_i,
+    surface_pressure_i,
+    consvar_i,
+    temperature_i,
+    geopotential_i,
+    gradp_e,
+    ugradp_i,
+    dmass_air_i,
+    Omega_i,
+)
 
 # lon-lat diagnostics
-ulon(interp, ulonlat_i) = interp(ulonlat_i.ulon)
-ulat(interp, ulonlat_i) = interp(ulonlat_i.ulat)
-pressure(interp, pressure_i) = interp(pressure_i)
-surface_pressure(interp, surface_pressure_i) = interp(surface_pressure_i)
-consvar(interp, consvar_i) = interp(consvar_i)
-temperature(interp, temperature_i) = interp(temperature_i)
-geopotential(interp, geopotential_i) = interp(geopotential_i)
-ugradp(interp, ugradp_i) = interp(ugradp_i)
+ulon(to_lonlat, ulonlat_i) = to_lonlat(ulonlat_i.ulon)
+ulat(to_lonlat, ulonlat_i) = to_lonlat(ulonlat_i.ulat)
+pressure(to_lonlat, pressure_i) = to_lonlat(pressure_i)
+surface_pressure(to_lonlat, surface_pressure_i) = to_lonlat(surface_pressure_i)
+consvar(to_lonlat, consvar_i) = to_lonlat(consvar_i)
+temperature(to_lonlat, temperature_i) = to_lonlat(temperature_i)
+geopotential(to_lonlat, geopotential_i) = to_lonlat(geopotential_i)
+ugradp(to_lonlat, ugradp_i) = to_lonlat(ugradp_i)
+dmass_air(to_lonlat, dmass_air_i) = to_lonlat(dmass_air_i)
+Omega(to_lonlat, Omega_i) = to_lonlat(Omega_i)
 
 # diagnostics on native grid
 
-ucov(state) = state.ucov
-mass_air(model, state) = rescale_mass(model, state.mass_air)
-mass_consvar(model, state) = rescale_mass(model, state.mass_consvar)
+ucov_e(state) = state.ucov
+mass_air_i(model, state) = rescale_mass(model, state.mass_air)
+mass_consvar_i(model, state) = rescale_mass(model, state.mass_consvar)
 rescale_mass(model, mass) = (model.planet.radius^-2) * mass
 
-surface_pressure_i(model, mass_air) =
-    model.vcoord.ptop .+ reshape(sum(mass_air; dims = 1), :)
+surface_pressure_i(model, mass_air_i) =
+    model.vcoord.ptop .+ reshape(sum(mass_air_i; dims = 1), :)
 
-consvar_i(mass_air, mass_consvar) = @. mass_consvar / mass_air
+consvar_i(mass_air_i, mass_consvar_i) = @. mass_consvar_i / mass_air_i
 
 temperature_i(model, pressure_i, consvar_i) =
     model.gas(:p, :consvar).temperature.(pressure_i, consvar_i)
 
-function pressure_i(model, mass_air)
-    # mass_air[ij, nz]
-    p = similar(mass_air)
+dmass_air_i(model, mass_air_i, ucov_e) = dmass_i(model, mass_air_i, ucov_e)
+dmass_consvar_i(model, mass_consvar_i, ucov_e) = dmass_i(model, mass_consvar_i, ucov_e)
+
+function pressure_i(model, mass_air_i)
+    # mass_air_i[ij, nz]
+    p = similar(mass_air_i)
     @with model.mgr,
     let ijrange = 1:size(p, 2)
         ptop, nz = model.vcoord.ptop, size(p, 1)
         for ij in ijrange
-            p[nz, ij] = ptop + mass_air[nz, ij] / 2
+            p[nz, ij] = ptop + mass_air_i[nz, ij] / 2
             for k = nz:-1:2
-                p[k-1, ij] = p[k, ij] + (mass_air[k, ij] + mass_air[k-1, ij]) / 2
+                p[k-1, ij] = p[k, ij] + (mass_air_i[k, ij] + mass_air_i[k-1, ij]) / 2
             end
         end
     end
@@ -73,7 +80,7 @@ function pressure_i(model, mass_air)
 end
 
 function ulonlat_i(model, state)
-    domain, planet, ucov = model.domain.layer, model.planet, state.ucov
+    domain, planet, ucov_e = model.domain.layer, model.planet, state.ucov
     lon, lat = domain.lon_i, domain.lat_i
     coslon, sinlon = cos.(lon), sin.(lon)
     coslat, sinlat = cos.(lat), sin.(lat)
@@ -85,22 +92,22 @@ function ulonlat_i(model, state)
     sinlon,
     coslat,
     sinlat
-    primal_lonlat_from_cov!(ulon, ulat, ucov, args...) do ij, ulon_ij, ulat_ij
+    primal_lonlat_from_cov!(ulon, ulat, ucov_e, args...) do ij, ulon_ij, ulat_ij
         lonlat_from_cov(ulon_ij, ulat_ij, lon[ij], lat[ij], planet)
     end
     return (; ulon, ulat)
 end
 
-function geopotential_i(model, mass_air, mass_consvar, pressure_i)
+function geopotential_i(model, mass_air_i, mass_consvar_i, pressure_i)
     Phi = similar(pressure_i, size(pressure_i) .+ (1, 0))
     @with model.mgr, let ijrange = axes(pressure_i, 2)
         vol = model.gas(:p, :consvar).specific_volume
         for ij in ijrange
             Phi[1, ij] = model.Phis[ij]
             for k in axes(pressure_i, 1)
-                consvar_ijk = mass_consvar[k, ij] / mass_air[k, ij]
+                consvar_ijk = mass_consvar_i[k, ij] / mass_air_i[k, ij]
                 v = vol(pressure_i[k, ij], consvar_ijk)
-                dPhi = mass_air[k, ij] * v
+                dPhi = mass_air_i[k, ij] * v
                 Phi[k+1, ij] = Phi[k, ij] + dPhi
             end
         end
@@ -108,8 +115,8 @@ function geopotential_i(model, mass_air, mass_consvar, pressure_i)
     return Phi
 end
 
-function gradp(model, ucov, pressure_i) # 1-form, at edges
-    gradp = similar(ucov)
+function gradp_e(model, ucov_e, pressure_i) # 1-form, at edges
+    gradp = similar(ucov_e)
     left_right = model.domain.layer.edge_left_right
 
     @with model.mgr, let (krange, ijrange) = axes(gradp)
@@ -123,23 +130,96 @@ function gradp(model, ucov, pressure_i) # 1-form, at edges
     return gradp
 end
 
-function ugradp_i(model, pressure_i, ucov, gradp) # scalar, primal mesh
+function ugradp_i(model, pressure_i, ucov_e, gradp_e) # scalar, primal mesh
     ugradp = similar(pressure_i)
-    domain, planet = model.domain.layer, model.planet
+    domain, radius = model.domain.layer, model.planet.radius
 
     @with model.mgr,
     let (krange, ijrange) = axes(ugradp)
         @inbounds for ij in ijrange
             deg = domain.primal_deg[ij]
             @unroll deg in 5:7 begin
-                st = stencil(dot_product, Val(deg), ij, radius, domain)
+                st = dot_product(Val(deg), ij, domain, radius)
                 for k in krange
-                    ugradp[k, ij] = dot_product(Val(deg), st, k, gradp, ucov)
+                    ugradp[k, ij] = dot_product(Val(deg), st, k, gradp_e, ucov_e)
                 end
             end
         end
     end
     return ugradp
+end
+
+function dmass_i(model, mass_i, ucov_e) # scalar, primal mesh
+    flux, dmass = similar(ucov_e), similar(mass_i)
+    domain, inv_rad2 = model.domain.layer, model.planet.radius^-2
+
+    @with model.mgr,
+    let (krange, ijrange) = axes(flux)
+        @inbounds for ij in ijrange
+            st = centered_flux(ij, domain)
+            for k in krange
+                flux[k, ij] = inv_rad2 * centered_flux(st, k, mass_i, ucov_e)
+            end
+        end
+    end
+
+    @with model.mgr,
+    let (krange, ijrange) = axes(dmass)
+        @inbounds for ij in ijrange
+            deg = domain.primal_deg[ij]
+            @unroll deg in 5:7 begin
+                st = divergence(Val(deg), ij, domain)
+                for k in krange
+                    dmass[k, ij] = -divergence(Val(deg), st, k, flux)
+                end
+            end
+        end
+    end
+
+    return dmass
+end
+
+function Omega_i(model, ugradp_i, dmass_air_i)    #= pressure_i, ugradPhi_i, mass_air_i, mass_consvar_i, dmass_consvar_i =#
+
+    # dmass is a scalar (O-form in kg/m²/s)
+    # consvar is a scalar (O-form in kg/m²/s)
+    Omega, Phi_dot = similar(ugradp_i), similar(ugradp_i)
+    dp_mid = Phi_dot # use Phi_dot as buffer for dp_mid
+
+    #    @with model.mgr,
+    let ijrange = axes(ugradp_i, 2)
+        nz = size(ugradp_i, 1)
+        volume = model.gas(:p, :consvar).volume_functions
+
+        for ij in ijrange
+            # top_down: dp, Omega
+            dp_top = zero(Omega[1, ij])
+            for k = nz:-1:1
+                dp_bot = dp_top + dmass_air_i[k, ij]
+                dp_mid[k, ij] = (dp_top + dp_bot) / 2
+                Omega[k, ij] = dp_mid[k, ij] + ugradp_i[k, ij]
+                dp_top = dp_bot
+            end
+            #=
+            # bottom-up: Phi_dot
+            dPhi = zero(Phi_dot[1, ij])
+            for k = 1:nz
+                consvar = mass_consvar_i[k, ij] / mass_air_i[k, ij]
+                mass_dconsvar = dmass_consvar_i[k, ij] - consvar * dmass_air_i[k, ij]
+                v, dv_dp, dv_dconsvar = volume(pressure_i[k, ij], consvar)
+                ddPhi =
+                    v * dmass_air[k, ij] +
+                    dv_dconsvar * mass_dconsvar +
+                    dv_dp * mass_air[k, ij] * dp_mid[k, ij]
+                # here we take care to write into Phi_dot *after* reading from dp_mid
+                Phi_dot[k, ij] = (dPhi + ddPhi / 2) + ugradPhi_i[k, ij]
+                dPhi += ddPhi
+            end
+            =#
+        end
+
+    end
+    return Omega
 end
 
 end # module
--- src/julia/voronoi_stencils.jl
+++ src/julia/voronoi_stencils.jl
@@ -1,25 +1,65 @@
 module Stencils
 
+using ManagedLoops: @unroll
+
 macro gen(expr)
-    code = :(
-        @inline Base.@propagate_inbounds @generated $expr
-    )
-    return esc(code)
+    esc(:(@inline Base.@propagate_inbounds @generated $expr))
 end
 
-#======================= stencils ======================#
-
-@gen dot_product(::Val{degree}, stencil, k, ucov::M, vcov::M) where {degree, M<:Matrix} = quote
-    inv_area, edges, hodges = stencil
-    @unroll inv_area * sum(hodges[e] * (ucov[k, edges[e]] * vcov[k, edges[e]]) for e = 1:$degree)
+macro inl(expr)
+    esc(:(@inline Base.@propagate_inbounds $expr))
 end
 
-@gen stencil(::typeof(dot_product), ::Val{degree}, ij, radius, domain) where degree = quote
-    inv_area = inv(radius * radius * domain.Ai[ij])
+#======================= dot product ======================#
+
+@gen dot_product(::Val{degree}, ij::Int, domain, radius) where {degree} =
+    quote
+        inv_area = inv(radius * radius * domain.Ai[ij])
+        edges = @unroll (domain.primal_edge[e, ij] for e = 1:$degree)
+        hodges = @unroll (domain.le_de[edges[e]] for e = 1:$degree)
+        return (; inv_area, edges, hodges)
+    end
+
+@gen dot_product(::Val{degree}, st::NamedTuple, k, ucov::M, vcov::M) where {degree,M<:AbstractMatrix} =
+    quote
+        inv_area, edges, hodges = st
+        @unroll inv_area *
+                sum(hodges[e] * (ucov[k, edges[e]] * vcov[k, edges[e]]) for e = 1:$degree)
+    end
+
+#======================= centered flux ======================#
+
+# le_de non-dimensional
+# ucov in m^2/s
+# mass in kg/m² (rho.dz)
+# => this flux in kg/s
+# rescaled to kg/s/m^2 by caller using metric factor
+# => div(flux) in kg/m^2/s
+
+@inl centered_flux(ij::Int, domain) =
+    ij, domain.edge_left_right[1, ij], domain.edge_left_right[2, ij], domain.le_de[ij]
+
+@inl function centered_flux(st::Tuple, k, mass::M, ucov::M) where {M<:AbstractMatrix}
+    ij, left, right, le_de = st
+    return le_de * ucov[k, ij] * (mass[k, left] + mass[k, right])/2
+end
+
+#========================= divergence =======================#
+
+# belongs to CFDomains
+# flux must be a contravariant vector density = 2-form in 3D space
+# in X/s for the flux of X
+
+@gen divergence(::Val{degree}, ij::Int, domain) where {degree} = quote
+    inv_area = inv(domain.Ai[ij])
     edges = @unroll (domain.primal_edge[e, ij] for e = 1:$degree)
-    hodges = @unroll (domain.le_de[edges[e]] for e = 1:$degree)
-    return inv_area, edges, hodges
+    signs = @unroll (domain.primal_ne[e, ij] for e=1:$degree)
+    return (; inv_area, edges, signs)
 end
 
+@gen divergence(::Val{degree}, st::NamedTuple, k, flux::AbstractMatrix) where degree = quote
+    (; inv_area, edges, signs) = st
+    return @unroll inv_area * sum(flux[k, edges[e]]*signs[e] for e = 1:$degree)
 end
 
+end #===== module ====#
