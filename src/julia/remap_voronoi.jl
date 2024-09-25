58168b96b4577d1c2c96e6d011fce0352384f42d
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-09-25 17:04:42 +0200

    Remap with mass coordinate

--- src/julia/remap_voronoi.jl
+++ src/julia/remap_voronoi.jl
@@ -2,7 +2,7 @@ module RemapVoronoi
 
 using MutatingOrNot: void, Void
 using ManagedLoops: @with, @vec
-using CFDomains: Stencils, VoronoiSphere
+using CFDomains: Stencils, VoronoiSphere, mass_coordinate
 using CFTransport: remap_fluxes!
 using ..RemapHPE: vanleer, remap_density!, remap_scalar!, update_mass!
 
@@ -17,19 +17,20 @@ function remap_staggered!(new, scratch, model, state, schemes)
     (; mass_consvar, ucov) = state
     (; mgr, vcoord, domain) = model
     vsphere, layout = domain.layer, domain.layout
-    metric_cov = model.planet.radius^2
-    metric_contra = inv(metric_cov)
 
     scheme_mq = schemes.scalar(:density, layout)
     scheme_u = schemes.momentum(:scalar, layout)
 
-    # We scale mass_air by the metric_contra because this is what pressure coordinates expect.
-    # This will become unnecessary after introducing mass coordinates.
-    mass_air = @. metric_contra * state.mass_air
+    metric_cov = model.planet.radius^2
+    mcoord = mass_coordinate(model.vcoord, metric_cov) # pressure coordinate => covariant mass coordinate
+
+    # Ensuring that new===state works requires that new_mass_air be a scratch array.
+    # Scratch space is not needed for new_air_consvar and new_ucov.
 
     # mass fluxes and new mass
+    mass_air = state.mass_air  # `mcoord` works with `mass_air` per unit surface on the unit sphere
     flux, new_mass_air =
-        remap_fluxes!(mgr, vcoord, layout, scratch.flux, new.mass_air,  #==# mass_air)
+        remap_fluxes!(mgr, mcoord, layout, scratch.flux, scratch.new_mass_air, mass_air)
 
     # vertical transport of densities
     new_mass_consvar, remap_consvar = remap_density!(
@@ -43,25 +44,36 @@ function remap_staggered!(new, scratch, model, state, schemes)
     )
 
     # vertical transport of momentum
-    flux_e, mass_e =
-        transfer_mass_flux!(mgr, scratch.flux_e, scratch.mass_e, ucov, mass_air, flux, vsphere)
-    new_ucov, remap_momentum = remap_scalar!(
+    flux_e, mass_e = transfer_mass_flux!(
         mgr,
-        scheme_u,
-        new.ucov,
-        scratch.remap_momentum,
+        scratch.flux_e,
+        scratch.mass_e,
         ucov,
-        mass_e,
-        flux_e,
+        mass_air,
+        flux,
+        vsphere,
     )
+    new_ucov, remap_momentum =
+        remap_scalar!(mgr, scheme_u, new.ucov, scratch.remap_momentum, ucov, mass_e, flux_e)
 
-    scratch = (; flux, flux_e, mass_e, remap_consvar, remap_momentum)
-    # revert scaling, see above
-    new = (mass_air = metric_cov*new_mass_air, mass_consvar = new_mass_consvar, ucov = new_ucov)
+    scratch = (; new_mass_air, flux, flux_e, mass_e, remap_consvar, remap_momentum)
+    new = (
+        mass_air = (@. new.mass_air = new_mass_air),
+        mass_consvar = new_mass_consvar,
+        ucov = new_ucov,
+    )
     return new, scratch
 end
 
-function transfer_mass_flux!(mgr, flux_e_, mass_e_, ucov, mass_air, flux, vsphere::VoronoiSphere)
+function transfer_mass_flux!(
+    mgr,
+    flux_e_,
+    mass_e_,
+    ucov,
+    mass_air,
+    flux,
+    vsphere::VoronoiSphere,
+)
     # interpolate mass and vertical mass flux to edges, where ucov lives
     mass_e = similar!(mass_e_, ucov)
     @with mgr, let (krange, ijrange) = axes(mass_e)
